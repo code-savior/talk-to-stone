f16ba9c3d5ab788d9426888735a7b1c25d49d8e8
ljy@kandaovr.com <ljy@kandaovr.com>
2023-06-30 16:47:12 +0800

    set lang to zh-CN

--- app/layout.tsx
+++ app/layout.tsx
@@ -33,7 +33,7 @@ interface RootLayoutProps {
 
 export default function RootLayout({ children }: RootLayoutProps) {
   return (
-    <html lang="en" suppressHydrationWarning>
+    <html lang="zh-CN" suppressHydrationWarning>
       <head />
       <body
         className={cn(
